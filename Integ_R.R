872397c2a56f7a36d5f949e76fa4fde71f26b473
chinbhagat <chinbhagat@gmail.com>
2013-10-29 10:25:03 -0400

    Integrated Code

---
+++ Integ_R.R
@@ -0,0 +1,79 @@
+GraphReadClass <- function(name) {
+  nc = list(
+    name = name,
+    nodes = character(),
+    edges = character(),
+    getEdges = function() {
+      return(nc$edges)
+    },
+    getNodes = function() {
+      return(nc$nodes)
+    }    
+  )
+  
+  nc$readFromFile<-function(fileName){
+    doc <- xmlTreeParse(fileName)
+    top <- xmlRoot(doc)
+    
+    xmlNodes <- xpathApply(top,"//project/node")
+    
+    nodeIds<-character()
+    for(i in 1:length(xmlNodes)){
+      nodeIds<-c(nodeIds,xmlGetAttr(xmlNodes[[i]],"id"))
+    }
+    
+    edges<-character()    
+    
+    xmlEdges <- xpathApply(top,"//project/edge")
+    
+    for(i in 1:length(xmlEdges)){
+      edges<-c(edges,xmlGetAttr(xmlEdges[[i]],"from"))
+      edges<-c(edges,xmlGetAttr(xmlEdges[[i]],"to"))      
+    }
+    
+    assign("nodes",nodeIds,envir=nc)
+    assign("edges",edges,envir=nc)
+  }
+  
+  nc <- list2env(nc)
+  class(nc) <- "GraphReadClass"
+  return(nc)
+}
+
+test <- GraphReadClass(name="Jason")
+fileName="C:\\Users\\chin_bhagat\\bioSample.xml"
+test$readFromFile(fileName)
+#test$getNodes()
+#test$getEdges()
+
+
+createEmptyGraph <- function(){
+  print("Creating Empty Graph")
+  g <- graph.empty()
+  return(g)
+}
+
+addVertexToGraph <- function(graph,vertexId){
+  print("Adding Vertex to graph")
+  graph <- graph + vertex(vertexId)
+  return(graph)
+}
+
+addEdgeToGraph <- function(graph,from,to){
+  print("Adding Edge to graph")
+  graph <- graph + edge(from,to)
+  return(graph)
+}
+addMultipleEdgesToGraph <- function(graph,edgeSet){
+  print("Adding Multiple Edges to graph")
+  graph <- graph + edges(edgeSet)
+  return(graph)
+}
+g <- createEmptyGraph()
+nodeSet <- c(test$getNodes())
+for(i in 1:length(nodeSet)){
+  g <- addVertexToGraph(g,nodeSet[i])
+}
+edgeSet <- c(test$getEdges())
+g <- addMultipleEdgesToGraph(g,edgeSet)
+get.edgelist(g)
